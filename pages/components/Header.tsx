ec50ee26605a71cb4f0f349c80ee05852b4ecf1e
sohil <sohilf88@gmail.com>
2022-09-13 16:57:56 +0530

    Nvabar jsx added

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,34 +1,56 @@
 import Link from "next/link"
 import {IoSearch} from "react-icons/io5"
 import { HiBell } from "react-icons/hi";
+import { useEffect,useState } from 'react';
 
+//bg-[#141414]
 const Header = () => {
-  return (
-    <div className="flex justify-between items-center bg-[#141414] text-white h-[72px] px-14 ">
+// function to set Sticky Navbar
+const [isScroll,setIsScroll]=useState(false);
+
+const handleScroll=()=>{
+    if(window.scrollY>0){
+        setIsScroll(true)
+    }else{
+        setIsScroll(false)
+    }
+};
+useEffect(()=>{
+    window.addEventListener("scroll",handleScroll);
+    return ()=>{
+        window.removeEventListener("scroll",handleScroll);
+    }
+    
+},[])
+// function to set Sticky Navbar ends here
+     
+return ( 
+    <div className="header-class">
         <span className="sm:hidden"><Link href="#">Browse</Link></span>
-        <ul className="sm:flex space-x-3 hidden ">
-            <li >
+        <ul className="sm:flex space-x-5 hidden  ">
+            <li className="hover:text-red-500 font-semibold" >
                 <Link href="#">Home</Link>
             </li>
-            <li>
+            <li className="hover:text-red-500">
                 <Link href="#">TV Shows</Link>
-            </li><li>
+            </li>
+            <li className="hover:text-red-500">
                 <Link href="#">Movies</Link>
-            </li><li>
+            </li>
+            <li className="hover:text-red-500">
                 <Link href="#">New & Popular</Link>
-            </li><li>
+            </li>
+            <li className="hover:text-red-500">
                 <Link href="#">My List</Link>
             </li>
         </ul>
         <div className="right-pane flex items-center gap-4 justify-center">
-            <IoSearch className="text-[25px] hidden sm:block"></IoSearch>
-            <span className="md:inline-block hidden">Kids</span>
-            <HiBell className="text-[25px]"></HiBell>
+            <IoSearch className="text-[25px] hidden sm:block hover:text-red-500"></IoSearch>
+            <span className="md:inline-block hidden hover:text-red-500">Kids</span>
+            <HiBell className="text-[25px] hover:text-red-500"></HiBell>
                 
         </div>
-        
-        
-        
+            
     </div>
   )
 }
