760d84a1ae086b9bf6560cfc9388c47794affe70
sohil <sohilf88@gmail.com>
2022-09-14 22:27:43 +0530

    Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,69 +4,22 @@ import requests from "./requests/requesturl"
 import Header from "./components/Header"
 import Banner from "./components/Banner"
 
+export const getServerSideprops =async()=>{
 
-export const getServerSideProps =async ()=>{
-  const [
-    fetchTrending,
-    fetchNetflixOriginals,
-    fetchTopRated,
-    fetchActionMovies,
-    fetchComedyMovies,
-    fetchHorrorMovies,
-    fetchRomanceMovies,
-    fetchDocumentaries,
-    fetchMostPopularMovies,
-    fetchUpcoming,
-  ]=await Promise.all([
-    fetch(requests.fetchTrending).then((response)=>response.json()),
-    fetch(requests.fetchNetflixOriginals).then((response)=>response.json()),
-    fetch(requests.fetchTopRated).then((response)=>response.json()),
-    fetch(requests.fetchActionMovies).then((response)=>response.json()),
-    fetch(requests.fetchComedyMovies).then((response)=>response.json()),
-    fetch(requests.fetchHorrorMovies).then((response)=>response.json()),
-    fetch(requests.fetchRomanceMovies).then((response)=>response.json()),
-    fetch(requests.fetchDocumentaries).then((response)=>response.json()),
-    fetch(requests.fetchMostPopularMovies).then((response)=>response.json()),
-    fetch(requests.fetchUpcoming).then((response)=>response.json()),
-  ]
-    )
-  
- return {
-  props:{
-  Trending:fetchTrending.results,
-  NetflixOriginals:fetchNetflixOriginals.results,
-  TopRated:fetchTopRated.results,
-  ActionMovies:fetchActionMovies.results,
-  ComedyMovies:fetchComedyMovies.results,
-  HorrorMovies:fetchHorrorMovies.results,
-  omanceMovies:fetchRomanceMovies.results,
-  Documentaries:fetchDocumentaries.results,
-  Upcoming:fetchUpcoming.results,
-  MostPopularMovie:fetchMostPopularMovies.results,
+  return { 
+    props:{
+
+    }
   }
- }
-} 
-const Home: NextPage = ({ 
-Trending,
-NetflixOriginals,
-TopRated,
-ActionMovies,
-ComedyMovies,
-HorrorMovies,
-RomanceMovies,
-DocumentMovies,
-MostPopularMovies,
-UpcomingMovies,
-}) => {
+}
+const Home = () => {
   return (
-    
+    <div>
+      <Header/>
       <main>
-        <Header/>
-        <Banner NetflixOriginals={NetflixOriginals}/>
-      
-    
+        <Banner/>
       </main>
-
+    </div>
   )
 }
 
--- pages/requests/requesturl.ts
+++ pages/requests/requesturl.ts
@@ -1,4 +1,4 @@
-const API_KEY=process.env.NEXT_API_KEYS;
+export const API_KEY=process.env.NEXT_API_KEY;
 export const BASE_URL="https://api.themoviedb.org/3";
 
 const requests ={
