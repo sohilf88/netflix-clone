04d640cb4661580d7c842ef1d484b7f03e74ede2
sohil <sohilf88@gmail.com>
2022-09-30 19:56:21 +0530

    Changes done

--- pages/requests/requesturl.ts
+++ pages/requests/requesturl.ts
@@ -1,5 +1,5 @@
 const API_KEY=process.env.NEXT_PUBLIC_API_KEY;
-export const BASE_URL="http://api.themoviedb.org/3";
+const BASE_URL="http://api.themoviedb.org/3";
 
 const requests ={
   fetchTrending: `${BASE_URL}/trending/all/week?api_key=${process.env.NEXT_PUBLIC_API_KEY}`,
