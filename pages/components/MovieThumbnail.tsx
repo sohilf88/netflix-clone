f455ab935e6571247a65f59a8f29128092a1125e
sohil <sohilf88@gmail.com>
2022-10-10 11:12:52 +0530

    added color

--- pages/components/MovieThumbnail.tsx
+++ pages/components/MovieThumbnail.tsx
@@ -4,6 +4,7 @@ import {baseUrlw500} from "../requests/baseuri"
 import Image from "next/image"
 import Tippy from '@tippyjs/react'
 import 'tippy.js/dist/tippy.css'
+import { transcode } from 'buffer'
 
 
 
@@ -14,7 +15,7 @@ type ImageProps={
 const MovieThumbnail = ({movie}:ImageProps) => {
       
   return (
-    <Tippy content=<strong>Movie- {movie.original_title || movie.original_name}</strong>  placement="bottom">
+    <Tippy content=<span>Movie- <strong style={{color:"#E50914"}}>{movie.original_title || movie.original_name}</strong></span>  placement="bottom">
     <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px]  md:hover:rounded md:hover:scale-105 overflow-hidden" >
        <Image 
        src={`${baseUrlw500}${movie.backdrop_path ||movie.poster_path}`} 
